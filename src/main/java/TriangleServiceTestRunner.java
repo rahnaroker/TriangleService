2b70228810470c5eebf4463493e8d55aaee0ea2d
a.cheryomukhin <acheremukhin@bellintegrator.com>
2020-10-18 15:17:57 +0300

    add test runner class

---
+++ src/main/java/TriangleServiceTestRunner.java
@@ -0,0 +1,46 @@
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+public class TriangleServiceTestRunner {
+
+    private static final Logger logger = LogManager.getLogger(TriangleServiceTestRunner.class);
+
+    TriangleServiceActions tsa = new TriangleServiceActions();
+    TriangleServiceDataGenerator tsdg = new TriangleServiceDataGenerator();
+    Response response;
+
+    int statusCode;
+    int countedPerimeter;
+    int countedArea;
+
+    public int FIRST_SIDE = tsdg.getFIRST_SIDE();
+    public int SECOND_SIDE = tsdg.getSECOND_SIDE();
+    public int THIRD_SIDE = tsdg.getTHIRD_SIDE();
+    public int EXPECTED_PERIMETER = tsdg.getEXPECTED_PERIMETER();
+    public int EXPECTED_AREA = tsdg.getEXPECTED_AREA();
+    public static int TRIANGLE_LIMIT = 10;
+
+    @Rule
+    public TestName name = new TestName();
+
+    @Before
+    public void cleanBefore() {
+        logger.debug("Running test: " + name.getMethodName());
+        tsa.deleteAllTriangles();
+        Assert.assertEquals(0, tsa.getTriangleIds().size());
+        tsdg.dataGenerator();
+    }
+
+    @After
+    public void cleanAfter() {
+        logger.debug("Ending test: " + name.getMethodName());
+        tsa = null;
+        response = null;
+    }
+}
--- src/main/java/TriangleServiceTests.java
+++ src/main/java/TriangleServiceTests.java
@@ -1,45 +1,7 @@
-import io.restassured.response.Response;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.*;
-import org.junit.rules.TestName;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class TriangleServiceTests {
-
-    private static final Logger logger = LogManager.getLogger(TriangleServiceTests.class);
-
-    TriangleServiceActions tsa = new TriangleServiceActions();
-    TriangleServiceDataGenerator tsdg = new TriangleServiceDataGenerator();
-    Response response;
-
-    int statusCode;
-    int countedPerimeter;
-    int countedArea;
-
-    public int FIRST_SIDE = tsdg.getFIRST_SIDE();
-    public int SECOND_SIDE = tsdg.getSECOND_SIDE();
-    public int THIRD_SIDE = tsdg.getTHIRD_SIDE();
-    public int EXPECTED_PERIMETER = tsdg.getEXPECTED_PERIMETER();
-    public int EXPECTED_AREA = tsdg.getEXPECTED_AREA();
-    public static int TRIANGLE_LIMIT = 10;
-
-    @Rule
-    public TestName name = new TestName();
-
-    @Before
-    public void cleanBefore() {
-        logger.debug("Running test: " + name.getMethodName());
-        tsa.deleteAllTriangles();
-        Assert.assertEquals(0, tsa.getTriangleIds().size());
-        tsdg.dataGenerator();
-    }
-
-    @After
-    public void cleanAfter() {
-        logger.debug("Ending test: " + name.getMethodName());
-        tsa = null;
-        response = null;
-    }
+public class TriangleServiceTests extends TriangleServiceTestRunner{
 
     @Test
     public void authSuccsess() {
